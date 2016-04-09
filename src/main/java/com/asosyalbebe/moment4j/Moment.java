85ea516e0c2aab50b193191a1558933dde16523c
Serdar Kuzucu <serdarkuzucu13@gmail.com>
2016-04-10 01:58:57 +0300

    Write javadoc until isBefore method.

--- src/main/java/com/asosyalbebe/moment4j/Moment.java
+++ src/main/java/com/asosyalbebe/moment4j/Moment.java
@@ -6,6 +6,10 @@ import java.util.Calendar;
 import java.util.Date;
 
 /**
+ * The class <code>Moment</code> represents a specific instant
+ * in time, with millisecond precision.
+ * <p>
+ * Moment also provides a very useful set of APIs to make it easy to manipulate dates in Java.
  * 
  * @author Serdar Kuzucu
  *
@@ -77,9 +81,10 @@ public class Moment implements Cloneable, Serializable, Comparable<Moment> {
     }
 
     /**
-     * Creates a moment instance by using the given array of values. 
+     * Creates a moment instance by using the given array of values.
+     * <p> 
      * The array must contain exactly 7 integers, which represents years, months, days, hours, minutes, seconds, and milliseconds.
-     * 
+     * <p>
      * For example, the following code can be used to create 31/01/2015 13:32:25.125
      * 
      * <pre>
@@ -115,10 +120,11 @@ public class Moment implements Cloneable, Serializable, Comparable<Moment> {
 
     /**
      * Sets the milliseconds.
+     * <p>
      * Accepts numbers from 0 to 999. If the range is exceeded, it will bubble up to the seconds.
      * 
      * @param milliseconds milliseconds to set.
-     * @return this Moment instance for chainability
+     * @return this Moment instance for chainability.
      */
     public Moment milliseconds(int milliseconds) {
 	return updateCalendarField(Calendar.MILLISECOND, milliseconds);
@@ -133,86 +139,249 @@ public class Moment implements Cloneable, Serializable, Comparable<Moment> {
 	return this.calendar.get(Calendar.MILLISECOND);
     }
 
+    /**
+     * Sets the seconds.
+     * <p>
+     * Accepts numbers from 0 to 59. If the range is exceeded, it will bubble up to the minutes.
+     * 
+     * @param seconds seconds to set.
+     * @return this Moment instance for chainability.
+     */
     public Moment seconds(int seconds) {
 	return updateCalendarField(Calendar.SECOND, seconds);
     }
 
+    /**
+     * Returns the seconds. The returned value is always in range from 0 to 59.
+     * 
+     * @return The seconds. The returned value is always in range from 0 to 59.
+     */
     public int seconds() {
 	return this.calendar.get(Calendar.SECOND);
     }
 
+    /**
+     * Sets the minutes.
+     * <p>
+     * Accepts numbers from 0 to 59. If the range is exceeded, it will bubble up to the hours.
+     * 
+     * @param minutes minutes to set.
+     * @return this Moment instance for chainability.
+     */
     public Moment minutes(int minutes) {
 	return updateCalendarField(Calendar.MINUTE, minutes);
     }
 
+    /**
+     * Returns the minutes. The returned value is always in range from 0 to 59.
+     * 
+     * @return The minutes. The returned value is always in range from 0 to 59.
+     */
     public int minutes() {
 	return this.calendar.get(Calendar.MINUTE);
     }
 
+    /**
+     * Sets the hour of the day.
+     * <p>
+     * Accepts numbers from 0 to 23. If the range is exceeded, it will bubble up to the days.
+     * 
+     * @param hours hours to set.
+     * @return this Moment instance for chainability.
+     */
     public Moment hours(int hours) {
 	return updateCalendarField(Calendar.HOUR_OF_DAY, hours);
     }
 
+    /**
+     * Returns the hour of day. The returned value is always in range from 0 to 23.
+     * 
+     * @return The hour of day. The returned value is always in range from 0 to 23.
+     */
     public int hours() {
 	return this.calendar.get(Calendar.HOUR_OF_DAY);
     }
 
+    /**
+     * Sets the day of month.
+     * <p>
+     * Accepts numbers from 0 to 31. If the range is exceeded, it will bubble up to the months.
+     * 
+     * @param dayOfMonth day of month to set.
+     * @return this Moment instance for chainability.
+     */
     public Moment dates(int dayOfMonth) {
 	return updateCalendarField(Calendar.DAY_OF_MONTH, dayOfMonth);
     }
 
+    /**
+     * Returns the day of month. The returned value is always in range from 0 to 31.
+     * 
+     * @return The day of month. The returned value is always in range from 0 to 31.
+     */
     public int dates() {
 	return this.calendar.get(Calendar.DAY_OF_MONTH);
     }
 
+    /**
+     * Sets the day of week.
+     * <p>
+     * Accepts numbers from 1 to 7. Sunday is 1, Saturday is 7.
+     * 
+     * @param dayOfWeek day of week to set. Calendar.SUNDAY, Calendar.MONDAY, etc. can be used.
+     * @return this Moment instance for chainability.
+     */
     public Moment days(int dayOfWeek) {
 	return updateCalendarField(Calendar.DAY_OF_WEEK, dayOfWeek);
     }
 
+    /**
+     * Returns the day of week. The returned value is always in range from 1 to 7.
+     * 
+     * @return The day of week. The returned value is always in range from 1 to 7.
+     */
     public int days() {
 	return this.calendar.get(Calendar.DAY_OF_WEEK);
     }
 
+    /**
+     * Sets the months.
+     * <p>
+     * Accepts numbers from 0 to 11. If the range is exceeded, it will bubble up to the year.
+     * <p>
+     * Note: Months are zero indexed, so January is month 0.
+     * <p>
+     * Calendar.JANUARY, Calendar.FEBRUARY, etc. can be used as argument.
+     * 
+     * @param months months to set
+     * @return this Moment instance for chainability.
+     */
     public Moment months(int months) {
 	return updateCalendarField(Calendar.MONTH, months);
     }
 
+    /**
+     * Returns the months. The returned value is always in range from 0 to 11.
+     * 
+     * @return The months. The returned value is always in range from 0 to 11.
+     */
     public int months() {
 	return this.calendar.get(Calendar.MONTH);
     }
 
+    /**
+     * Sets the years.
+     * 
+     * @param years years to set.
+     * @return this Moment instance for chainability.
+     */
     public Moment years(int years) {
 	return updateCalendarField(Calendar.YEAR, years);
     }
 
+    /**
+     * Returns the years.
+     * 
+     * @return The years.
+     */
     public int years() {
 	return this.calendar.get(Calendar.YEAR);
     }
 
+    /**
+     * Sets the day of the year.
+     * <p>
+     * Accepts numbers from 1 to 366. If the range is exceeded, it will bubble up to the years.
+     * 
+     * @param dayOfYear day of year to set. Accepts numbers from 1 to 366. If the range is exceeded, it will bubble up to the years.
+     * @return this Moment instance for chainability.
+     */
     public Moment dayOfYear(int dayOfYear) {
 	return updateCalendarField(Calendar.DAY_OF_YEAR, dayOfYear);
     }
 
+    /**
+     * Returns the day of year. The returned value is always in range from 1 to 366.
+     * 
+     * @return The returned value is always in range from 1 to 366.
+     */
     public int dayOfYear() {
 	return this.calendar.get(Calendar.DAY_OF_YEAR);
     }
 
+    /**
+     * Sets the given Calendar field to the given value.
+     * 
+     * <pre>
+     * {@code
+     * Moment moment = Moment.moment().set(Calendar.YEAR, 2012);
+     * }
+     * </pre>
+     * 
+     * @param calendarField field to set. For example, to update month field, use Calendar.MONTH
+     * @param value value to set.
+     * @return this Moment instance for chainability.
+     */
     public Moment set(int calendarField, int value) {
 	return updateCalendarField(calendarField, value);
     }
 
+    /**
+     * Returns the value of the given Calendar field. For example, to get day of month, use Calendar.DAY_OF_MONTH or Calendar.DATE
+     * 
+     * @param calendarField the calendar field whose value will be returned.
+     * @return the value of the given Calendar field.
+     */
     public int get(int calendarField) {
 	return this.calendar.get(calendarField);
     }
 
+    /**
+     * Adds the specified amount of time to the given calendar field, based on the wrapped calendar's rules.
+     * For example, to add 5 days to the current time of the moment, you can achieve it by calling:
+     * <p><code>add(5, Calendar.DAY_OF_MONTH)</code>.
+     *
+     * @param value the amount of date or time to be added to the field.
+     * @param calendarField the calendar field.
+     * @return this Moment instance for chainability.
+     */
     public Moment add(int value, int calendarField) {
 	return addCalendarField(calendarField, value);
     }
 
+    /**
+     * Subtracts the specified amount of time to the given calendar field, based on the wrapped calendar's rules.
+     * For example, to subtract 5 days from the current time of the moment, you can achieve it by calling:
+     * <p><code>subtract(5, Calendar.DAY_OF_MONTH)</code>.
+     *
+     * @param value the amount of date or time to be subtracted from the field.
+     * @param calendarField the calendar field.
+     * @return this Moment instance for chainability.
+     */
     public Moment subtract(int value, int calendarField) {
 	return addCalendarField(calendarField, -value);
     }
 
+    /**
+     * <p>Mutates the original moment by setting it to the start of a unit of time.</p>
+     * 
+     * <pre>
+     * moment().startOf(Calendar.YEAR);
+     * moment().startOf(Calendar.MONTH);
+     * moment().startOf(Calendar.HOUR);
+     * </pre>
+     * 
+     * <p>The above methods are essentially the same as the following.</p>
+     * 
+     * <pre>
+     * moment().months(0).days(1).hours(0).minutes(0).seconds(0).milliseconds(0);
+     * moment().days(1).hours(0).minutes(0).seconds(0).milliseconds(0);
+     * moment().minutes(0).seconds(0).milliseconds(0);
+     * </pre>
+     * 
+     * @param calendarField unit of time whose start time will be set to this moment instance.
+     * @return this Moment instance for chainability.
+     */
     public Moment startOf(int calendarField) {
 	switch (calendarField) {
 	case Calendar.YEAR:
@@ -248,6 +417,26 @@ public class Moment implements Cloneable, Serializable, Comparable<Moment> {
 	return this;
     }
 
+    /**
+     * <p>Mutates the original moment by setting it to the end of a unit of time.</p>
+     * 
+     * <pre>
+     * moment().endOf(Calendar.YEAR);
+     * moment().endOf(Calendar.MONTH);
+     * moment().endOf(Calendar.HOUR);
+     * </pre>
+     * 
+     * <p>The above methods are essentially the same as the following.</p>
+     * 
+     * <pre>
+     * moment().months(11).days(31).hours(23).minutes(59).seconds(59).milliseconds(999);
+     * moment().days(31).hours(23).minutes(59).seconds(59).milliseconds(999);
+     * moment().minutes(59).seconds(59).milliseconds(999);
+     * </pre>
+     * 
+     * @param calendarField unit of time whose end time will be set to this moment instance.
+     * @return this Moment instance for chainability.
+     */
     public Moment endOf(int calendarField) {
 	if (calendarField == Calendar.MILLISECOND) {
 	    return this;
@@ -256,6 +445,18 @@ public class Moment implements Cloneable, Serializable, Comparable<Moment> {
 	return this.startOf(calendarField).add(1, calendarField).subtract(1, Calendar.MILLISECOND);
     }
 
+    /**
+     * Returns the String representation of this moment instance in given date pattern.
+     * The pattern must obey java.text.SimpleDateFormat's rules.
+     * <p>
+     * 
+     * <pre>
+     * moment().format("yyyy/MM/dd HH:mm:ss.SSS");
+     * </pre>
+     * 
+     * @param pattern the pattern which will be used to format the date
+     * @return the String representation of this moment instance in given date pattern.
+     */
     public String format(String pattern) {
 	try {
 	    SimpleDateFormat format = new SimpleDateFormat(pattern);
@@ -265,14 +466,31 @@ public class Moment implements Cloneable, Serializable, Comparable<Moment> {
 	}
     }
 
+    /**
+     * Returns a Date object which is created by using date and time of this moment instance
+     * 
+     * @return a Date object which is created by using date and time of this moment instance
+     * @see java.util.Date
+     * @see java.util.Calendar#getTime()
+     */
     public Date toDate() {
 	return this.calendar.getTime();
     }
 
+    /**
+     * Returns a Calendar object which is created by using date and time of this moment instance
+     * 
+     * @return a Calendar object which is created by using date and time of this moment instance
+     * @see java.util.Calendar
+     */
     public Calendar toCalendar() {
 	return (Calendar) this.calendar.clone();
     }
 
+    /**
+     * TODO Write JavaDoc
+     * @return
+     */
     public int[] toArray() {
 	int[] array = new int[7];
 	array[0] = this.calendar.get(Calendar.YEAR);
@@ -285,31 +503,88 @@ public class Moment implements Cloneable, Serializable, Comparable<Moment> {
 	return array;
     }
 
+    /**
+     * Returns this Moment's time value in milliseconds.
+     * 
+     * @return the time as UTC milliseconds from the epoch.
+     * @see java.util.Date#getTime()
+     * @see java.util.Calendar#getTimeInMillis()
+     */
     public long valueOf() {
 	return this.calendar.getTimeInMillis();
     }
 
+    /**
+     * Returns this Moment's time value in seconds.
+     * <p>
+     * Simply divides milliseconds to 1000.
+     * 
+     * @return the time as UTC seconds from the epoch.
+     * @see java.util.Date#getTime()
+     * @see java.util.Calendar#getTimeInMillis()
+     */
     public long unix() {
 	return valueOf() / 1000;
     }
 
+    /**
+     * Creates a new Moment instance by copying this Moment instance's time value.
+     * 
+     * @return a new Moment instance which is created by copying this Moment instance's time value.
+     */
     @Override
     public Moment clone() {
 	return new Moment((Calendar) this.calendar.clone());
     }
 
+    /**
+     * Returns true if this moment instance is before the specified moment instance
+     *  
+     * @param moment moment to check
+     * @return <code>true</code> if and only if the instant of time represented by this <tt>Moment</tt>
+     * object is strictly earlier than the instant represented by the given moment parameter;
+     * <code>false</code> otherwise.
+     * @see java.util.Date#before(Date)
+     */
     public boolean isBefore(Moment moment) {
 	return isBefore(moment.valueOf());
     }
 
+    /**
+     * Returns true if this moment instance is before the specified date instance
+     *  
+     * @param date date to check
+     * @return <code>true</code> if and only if the instant of time represented by this <tt>Moment</tt>
+     * object is strictly earlier than the instant represented by the given date parameter;
+     * <code>false</code> otherwise.
+     * @see java.util.Date#before(Date)
+     */
     public boolean isBefore(Date date) {
 	return isBefore(date.getTime());
     }
 
+    /**
+     * Returns true if this moment instance is before the specified calendar instance
+     *  
+     * @param calendar calendar to check
+     * @return <code>true</code> if and only if the instant of time represented by this <tt>Moment</tt>
+     * object is strictly earlier than the instant represented by the given calendar parameter;
+     * <code>false</code> otherwise.
+     * @see java.util.Date#before(Date)
+     */
     public boolean isBefore(Calendar calendar) {
 	return isBefore(calendar.getTimeInMillis());
     }
 
+    /**
+     * Returns true if this moment instance is before the specified time as UTC milliseconds from the epoch.
+     *  
+     * @param milliseconds time as UTC milliseconds from the epoch to compare with this moment instance.
+     * @return <code>true</code> if and only if the instant of time represented by this <tt>Moment</tt>
+     * object is strictly earlier than the instant represented by the given long parameter;
+     * <code>false</code> otherwise.
+     * @see java.util.Date#before(Date)
+     */
     public boolean isBefore(long milliseconds) {
 	return this.valueOf() < milliseconds;
     }
@@ -496,6 +771,7 @@ public class Moment implements Cloneable, Serializable, Comparable<Moment> {
 
     /**
      * Returns true if that year is a leap year, and false if it is not.
+     * 
      * @return true if that year is a leap year, and false if it is not.
      */
     public boolean isLeapYear() {
@@ -563,6 +839,7 @@ public class Moment implements Cloneable, Serializable, Comparable<Moment> {
 
     /**
      * Returns true if the given year is a leap year, and false if it is not.
+     * 
      * @param year year to control if it is leap or not
      * @return true if the given year is a leap year, and false if it is not.
      */
